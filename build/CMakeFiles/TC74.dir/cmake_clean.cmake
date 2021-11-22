678aafd22af5174d20f987f9687f977f609a90a8
simonweytens <simon.weytens@student.vives.be>
2021-11-22 17:44:11 +0100

    first commit

---
+++ build/CMakeFiles/TC74.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TC74.dir/main.c.o"
+  "TC74"
+  "TC74.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TC74.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
